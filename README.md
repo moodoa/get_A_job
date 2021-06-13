4b30e5f874859df296b7c79f8e6aa327f4512a58
moodoa <39092862+moodoa@users.noreply.github.com>
2021-06-14 01:41:37 +0800

    <feat>getAjob file

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # get_A_job
-用 python 自動在 104 上投遞工作
+用程式自動在 104 上投遞工作
+
+![alt text](https://resumecheetah.com/wp-content/uploads/2020/01/find-a-job-3.jpg)
+
+## __init__
+* keyword : 想找的工作關鍵字 e.g., python。 
+* page : 想讓程式幫你投幾頁工作。
+* username : 104 上的帳號。 
+* password : 104 上的密碼。 
+* intro_idx : 自我推薦信，輸入 0 為預設，1 ~ 3 為自訂推薦信 1 ~ 3。
+
+## _get_all_submit_url
+* 收集所有符合條件之工作，回傳值為工作投遞頁面之連結。
+
+## submit_job
+* 使用 selenium 投遞工作。
+* 若有今日已投遞之工作將忽略。
+
+## Requirements
+python 3
+
+## Usage
+
+```
+if __name__ == "__main__":
+    test = GETAJOB("工作關鍵字", 頁數, "帳號", "密碼", 推薦信編號)
+    all_submit_url = test._get_all_submit_url()
+    test.submit_job(all_submit_url)
+
+```
+## Installation
+`pip install -r requriements.txt`。
---
+++ getAjob.py
@@ -0,0 +1,68 @@
+import re
+import requests
+from time import sleep
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+
+class GETAJOB:
+    def __init__(self, keyword, page, username, password, intro_idx):
+        self.keyword = keyword
+        self.page = page
+        self.username = username
+        self.password = password
+        self.intro_idx = intro_idx
+
+    def _get_all_submit_url(self):
+        all_submit_url = []
+        pattern = r".+/(.+)\?.+"
+        headers = {"Referer": "https://www.104.com.tw/jobs/search/?ro=0&keyword=python%20&expansionType=area,spec,com,job,wf,wktm&jobsource=2018indexpoc"}
+        for p in range(1, self.page+1):
+            data = requests.get(f"https://www.104.com.tw/jobs/search/list?ro=0&kwop=7&keyword={self.keyword}%20&expansionType=area%2Cspec%2Ccom%2Cjob%2Cwf%2Cwktm&order=12&asc=0&page={p}&mode=s&jobsource=2018indexpoc", headers=headers).json()
+            data = data["data"]
+            if data["list"]:
+                for single_job in data["list"]:
+                    job_url = single_job["link"]["job"]
+                    cust_url = single_job["link"]["cust"]
+                    job = re.findall(pattern, job_url)[0]
+                    cust = re.findall(pattern, cust_url)[0]
+                    if job and cust:
+                        url = f"https://m.104.com.tw/apply/step1/{cust}/{job}?jobsource=m104"
+                        all_submit_url.append(url)
+        return all_submit_url
+    
+    def submit_job(self, all_submit_url):
+        driver = webdriver.Chrome(".\chromedriver.exe")
+        driver.get(all_submit_url[0])
+        sleep(2)
+        id_code = driver.find_element_by_id("username")
+        id_code.send_keys(self.username)
+        sleep(1)
+        password = driver.find_element_by_id("password")
+        password.send_keys(self.password)
+        sleep(1)
+        login = driver.find_element_by_id("submitBtn")
+        login.click()
+        sleep(3)
+        try:
+            select = Select(driver.find_element_by_id("applyCover"))
+            select.select_by_index(self.intro_idx)
+            sleep(1)
+            submit = driver.find_element_by_class_name("submit")
+            submit.click()
+        except:
+            pass
+        for idx in range(1, len(all_submit_url)):
+            driver.get(all_submit_url[idx])
+            sleep(0.5)
+            try:
+                select = Select(driver.find_element_by_id("applyCover"))
+                select.select_by_index(self.intro_idx)
+                sleep(0.5)
+                submit = driver.find_element_by_class_name("submit")
+                submit.click()
+            except:
+                pass
+
+
+
